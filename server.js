44cb7f95a9959ed59f0d1034a1521793d3274e14
4drea4 <bakerand@outlook.com>
2026-01-12 14:51:31 -0600

    started writing the app.get

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 
+// In server.js, create a new GET route at the path /api/fun-fact.
+// This route will be responsible for fetching the data and sending it to the client.
+
+app.get("/api/fun-fact", async (req,res) => {
+    
+}
+)
 
 
 app.listen(PORT, ()=>{
